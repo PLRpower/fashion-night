d6a625b1a8083543ec378dc63d9ac9d9bc6bd5b0
plr <paul.thomas.stras@gmail.com>
2024-12-01 11:51:30 +0100

    Correction orthographe & bug vidéo

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 // Références aux éléments du DOM
 const audio = ref(null); // Référence à l'élément audio
 const play = ref(null); // Référence au bouton play
 const pause = ref(null); // Référence au bouton pause
+const video = ref(null); // Référence à l'élément vidéo
 
 // État de lecture
 const isPlaying = ref(false);
@@ -35,6 +36,18 @@ async function submitForm() {
 
 // Utilisation de useFetch (si nécessaire)
 const { data } = await useFetch('https://api.tnptrading.fr/email-fashion-night');
+
+// Fonction pour vérifier et démarrer la lecture de la vidéo
+const playVideoIfNotPlaying = () => {
+  if (video.value && video.value.paused) {
+    video.value.play();
+  }
+};
+
+// Lancer la vidéo à la fin du chargement
+onMounted(() => {
+  playVideoIfNotPlaying();
+});
 </script>
 
 <template>
@@ -44,7 +57,7 @@ const { data } = await useFetch('https://api.tnptrading.fr/email-fashion-night')
   <h1 class="text-end me-6 text-6xl lg:text-8xl mt-8 cardinal">the fashion night</h1>
   <div class="flex justify-center mt-16">
     <div class="bg-black movie">
-      <video class="w-full h-full" src="/img/video.mp4" autoplay muted loop playsinline></video>
+      <video class="w-full h-full" src="/img/video.mp4" ref="video" autoplay muted loop playsinline></video>
     </div>
     <nuxt-img class="z-10 w-11/12 lg:w-2/3" src="/img/ecran.webp"></nuxt-img>
   </div>
@@ -60,7 +73,7 @@ const { data } = await useFetch('https://api.tnptrading.fr/email-fashion-night')
       <input class="h-12 w-64 lg:w-96 text-center text-xl border-b-2 border-gray-400 cardinal outline-none focus:border-black" type="email"
              placeholder="votre adresse email" id="email" name="email" v-model="email">
     </div>
-    <p class="text-center mb-4">*Votre email est uniquement <span class="text-nowrap">utilisée pour vous contacter.</span><br>Elle n'est pas partagée avec des tiers.</p>
+    <p class="text-center mb-4">*Votre email est uniquement <span class="text-nowrap">utilisé pour vous contacter.</span><br>Il n'est pas partagé avec des tiers.</p>
     <div class="flex justify-center gap-x-4 mb-32">
       <button type="submit" class="align-middle self-center justify-center h-12 px-4 bg-black text-white text-xl cardinal">Valider</button>
     </div>
