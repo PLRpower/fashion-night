8d6881b139fea689c9f729e8a67fe4f4c506a42e
plr <paul.thomas.stras@gmail.com>
2025-01-28 19:48:38 +0100

    Ajout du lookbook

--- pages/index.vue
+++ pages/index.vue
@@ -64,8 +64,81 @@ onMounted(() => {
   <h2 class="text-center text-5xl mt-32 cardinal mx-2">Save the date :</h2>
   <p class="text-center text-4xl mt-4 cardinal">Calendrier officiel, 22 mars 2025</p>
 
-  <h1 class="cardinal text-center text-5xl mt-32 mb-4 mx-2">Lookbook à venir</h1>
-  <p class="cardinal text-4xl text-center mb-32">Restez connectés</p>
+  <h1 class="cardinal text-center text-5xl mt-32 mb-4 mx-2">Lookbook</h1>
+  <div class="grid grid-cols-2 gap-2 sm:grid-cols-4 mx-2">
+    <div class="flex flex-col gap-2">
+      <nuxt-img class="w-full" src="/img/lookbook/2.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/3.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/4.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/5.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/6.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/7.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/8.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/9.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/10.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/11.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/12.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/13.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/14.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/50.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/1.jpg" loading="lazy"></nuxt-img>
+    </div>
+    <div class="flex flex-col gap-2">
+      <nuxt-img class="w-full" src="/img/lookbook/16.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/17.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/18.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/19.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/20.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/21.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/22.webp" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/23.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/24.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/25.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/26.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/27.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/44.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/28.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/29.jpg" loading="lazy"></nuxt-img>
+    </div>
+    <div class="flex flex-col gap-2">
+      <nuxt-img class="w-full" src="/img/lookbook/30.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/31.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/32.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/33.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/34.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/35.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/36.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/37.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/38.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/39.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/40.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/41.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/42.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/43.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/45.jpg" loading="lazy"></nuxt-img>
+    </div>
+    <div class="flex flex-col gap-2">
+      <nuxt-img class="w-full" src="/img/lookbook/15.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/46.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/47.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/48.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/49.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/51.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/52.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/53.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/54.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/55.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/56.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/57.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/58.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/59.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/60.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/61.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/62.jpg" loading="lazy"></nuxt-img>
+      <nuxt-img class="w-full" src="/img/lookbook/63.jpg" loading="lazy"></nuxt-img>
+    </div>
+
+  </div>
 
   <h2 class="text-center text-4xl lg:text-5xl mx-2 mt-32 cardinal">Préréservez dès maintenant :</h2>
   <form @submit.prevent="submitForm" v-show="!send">
@@ -89,7 +162,6 @@ onMounted(() => {
   </div>
 
   <audio class="fixed bottom-0 right-0" src="/music.mp3" ref="audio"></audio>
-
 
 </template>
 
